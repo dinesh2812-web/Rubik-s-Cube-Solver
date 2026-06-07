9b1420dc1229f58e75bc101647dd1a11a6e09526
dinesh2812-web <akepatidineshprasad2812@gmail.com>
2026-06-07 18:06:32 +0530

    Update DFSSolver.h

--- DFSSolver.h
+++ DFSSolver.h
@@ -2,53 +2,70 @@
 #include <bits/stdc++.h>
 #include "../Model/RubiksCube.h"
 
-#ifndef RUBIKS_CUBE_SOLVER_DFSSOLVER_H
-#define RUBIKS_CUBE_SOLVER_DFSSOLVER_H
+#ifndef RUBIKS_CUBE_SOLVER_BFSSOLVER_H
+#define RUBIKS_CUBE_SOLVER_BFSSOLVER_H
 
-class DFSSolver {
+class BFSSolver {
 private:
     vector<RubiksCube::MOVE> moves;
-    int max_search_depth;
+    
+    map<RubiksCube3d, bool> visited;
+    map<RubiksCube3d, RubiksCube::MOVE> move_done;
 
-    // DFS code to find the solution (helper function)
-    bool dfs(int dep) {
-        // Base Cases
-        if (rubiksCube.isSolved()) return true;
-        if (dep > max_search_depth) return false;
+    // Performs breadth-first-search and returns a solved Rubik's Cube
+    RubiksCube3d bfs() {
+        queue<RubiksCube3d> q;
+        q.push(rubiksCube);
+        visited[rubiksCube] = true;
 
-        // Explore all 18 possible face-rotation configurations
-        for (int i = 0; i < 18; i++) {
-            RubiksCube::MOVE current_move = RubiksCube::MOVE(i);
-
-            rubiksCube.move(current_move);
-            moves.push_back(current_move);
-
-            // Recurse deeper into the search tree
-            if (dfs(dep + 1)) return true;
-
-            // Backtrack: Undo the move if it didn't lead to a solution
-            moves.pop_back();
-            rubiksCube.invert(current_move);
+        while (!q.empty()) {
+            RubiksCube3d node = q.front();
+            q.pop();
+            
+            if (node.isSolved()) {
+                return node;
+            }
+            
+            for (int i = 0; i < 18; i++) {
+                auto curr_move = RubiksCube::MOVE(i);
+                node.move(curr_move);
+                
+                if (!visited[node]) {
+                    visited[node] = true;
+                    move_done[node] = curr_move;
+                    q.push(node);
+                }
+                
+                node.invert(curr_move); // Backtrack the temporary mutation
+            }
         }
-        return false;
+        return rubiksCube;
     }
 
 public:
-    // Concrete 3D Cube instance instead of generic typename 'T'
-    RubiksCube3d rubiksCube; 
+    // Explicit 3D Cube instance
+    RubiksCube3d rubiksCube;
 
-    // Constructor updated to accept the 3D representation directly
-    DFSSolver(RubiksCube3d _rubiksCube, int _max_search_depth = 8) {
+    BFSSolver(RubiksCube3d _rubiksCube) {
         rubiksCube = _rubiksCube;
-        max_search_depth = _max_search_depth;
     }
 
-    // Triggers the solver and returns the winning move sequence
+    // Performs BFS and returns the vector of moves done to solve the cube
     vector<RubiksCube::MOVE> solve() {
-        moves.clear(); // Clear any previous runs
-        dfs(1);
+        RubiksCube3d solved_cube = bfs();
+        assert(solved_cube.isSolved());
+        
+        RubiksCube3d curr_cube = solved_cube;
+        while (!(curr_cube == rubiksCube)) {
+            RubiksCube::MOVE curr_move = move_done[curr_cube];
+            moves.push_back(curr_move);
+            curr_cube.invert(curr_move);
+        }
+        
+        rubiksCube = solved_cube;
+        reverse(moves.begin(), moves.end());
         return moves;
     }
 };
 
-#endif //RUBIKS_CUBE_SOLVER_DFSSOLVER_H
+#endif //RUBIKS_CUBE_SOLVER_BFSSOLVER_H
